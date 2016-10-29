3fa23580fccd9c7abee298719f35261c580aa448
Juan <valdes.juan@outlook.es>
2016-10-28 23:23:10 -0300

    ña

---
+++ Assets/Script/Ghost.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ghost : MonoBehaviour {
+    public Rigidbody2D rigid;
+    public Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
