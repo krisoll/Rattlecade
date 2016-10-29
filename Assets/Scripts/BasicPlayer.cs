3b0cb0d7ba99408f2dd623b3ced2fa2102b98a5e
krisoll <benitez.diego2@gmail.com>
2016-10-29 00:03:19 -0300

    Ghost y mierdas

---
+++ Assets/Scripts/BasicPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Rewired;
+public class BasicPlayer : MonoBehaviour {
+    public int playerID;
+    public Rigidbody2D rigid;
+    public float velocity;
+    private Player rePlayer;
+    private float horizontal;
+	// Use this for initialization
+	void Start () {
+        rePlayer = ReInput.players.GetPlayer(playerID);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        horizontal = rePlayer.GetAxisRaw("Horizontal");
+        rigid.velocity = new Vector2(horizontal * velocity, rigid.velocity.y);
+	}
+}
