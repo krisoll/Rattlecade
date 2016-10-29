4032687878a85f4375e950c1fe7d0ac33f70657f
Juan <valdes.juan@outlook.es>
2016-10-29 11:36:16 -0300

    bullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+    public Rigidbody2D rigid;
+    public Animator anim;
+    public float velocity;
+    public int damage;
+
+	// Use this for initialization
+	void Start () {
+        rigid.velocity = Quaternion.Euler(0, 0, transform.eulerAngles.z) * (Vector2.right * velocity);
+	}
+	
+    void OnTriggerEnter(Collider col)
+    {
+        rigid.velocity = Vector2.zero;
+        col.SendMessage("Damage", damage, SendMessageOptions.DontRequireReceiver);
+        col.SendMessage("Die", SendMessageOptions.DontRequireReceiver);
+        anim.SetTrigger("Hit");
+    }
+
+    void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
